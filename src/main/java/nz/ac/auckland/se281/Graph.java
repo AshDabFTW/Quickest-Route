d75ed4d08678dd56bb040a876e8ca1b1ae01867a
Ashkan Azarkish <aaza463@aucklanduni.ac.nz>
2024-05-21 11:20:08 +1200

    implemented code in graph class to make the graph

--- src/main/java/nz/ac/auckland/se281/Graph.java
+++ src/main/java/nz/ac/auckland/se281/Graph.java
@@ -1,5 +1,23 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Graph {
-  
+  private Map<Country, List<Country>> adjNodes;
+
+  public Graph() {
+    this.adjNodes = new HashMap<>();
+  }
+
+  public void addNode(Country node) {
+    adjNodes.putIfAbsent(node, new ArrayList<>());
+  }
+
+  public void addEdge(Country node1, Country node2) {
+    adjNodes.get(node1).add(node2);
+    adjNodes.get(node2).add(node1);
+  }
 }
